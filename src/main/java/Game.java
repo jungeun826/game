69757a976a4ae08ff36d4524dfb127034aa31454
jungeun <jungeun826@nate.com>
2013-11-04 20:37:27 +0900

    test6

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,20 +1,23 @@
 
 public class Game {
-	int rolls[] = new int[20];
+	int rolls[];
 	int currentIndex= 0;
 
-	public void roll(int pins) {
-		rolls[currentIndex++]= pins; 
+	public void roll(final int pins) 
+	{
+		rolls[currentIndex++]= pins;
+		rolls = new int[20];
 	}
 
-	public int score() {
+	public int score() 
+	{
 		int score = 0; 
 
-		for(int frame = 0; frame < 10; frame++){
+		for(int frame = 0; frame < 10; frame++)
+		{
 			if(isStrike(frame)){
 				score += 10 + frameScore(frame+1);
-			}
-			else if(isSpare(frame)){
+			}else if(isSpare(frame)){
 				score += 10 + bonusScoreOfSpare(frame);
 			}else{
 				score += frameScore(frame);
@@ -23,19 +26,19 @@ public class Game {
 		return score;
 	}
 
-	private boolean isStrike(int frame) {
+	private boolean isStrike(final int frame) {
 		return rolls[frame*2] == 10;
 	}
 
-	private int bonusScoreOfSpare(int frame) {
+	private int bonusScoreOfSpare(final int frame) {
 		return rolls[2*(frame+1)];
 	}
 
-	private boolean isSpare(int frame) {
+	private boolean isSpare(final int frame) {
 		return frameScore(frame) == 10;
 	}
 
-	private int frameScore(int frame) {
+	private int frameScore(final int frame) {
 		return rolls[2*frame]+rolls[2*frame+1];
 	}
 
